5051816e1237119af9b5ae576723e3ba3ae4836d
Anton Goroshkin <neobht@sibsau.ru>
2016-12-27 17:25:39 +0700

    add alt

---
+++ alt/9.0/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# set the postgres database host, port, user and password
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+# pass them as arguments to the odoo process
+DB_ARGS=("--db_user" $USER "--db_password" $PASSWORD "--db_host" $HOST "--db_port" $PORT)
+
+case "$1" in
+	-- | openerp-server)
+		shift
+		exec openerp-server "${DB_ARGS[@]}" "$@"
+		;;
+	-*)
+		exec openerp-server "${DB_ARGS[@]}" "$@"
+		;;
+	*)
+		exec "$@"
+esac
+
+exit 1
